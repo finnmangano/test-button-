261181c5963411ba3e95444b7881f7094db8550d
finnmangano <finnmangano3@gmail.com>
2024-11-24 15:38:01 +0800

    Updated script

--- script.js
+++ script.js
@@ -2,11 +2,12 @@
     const colors = ["red", "orange", "yellow", "green", "lightblue", "darkblue", "pink", "purple", "white"];
     let colorIndex = 0;
 
+    const bodyElement = document.getElementById("body");
+
     function changeColor() {
-        const col = document.getElementById("body");
         if (colorIndex >= colors.length) {
             colorIndex = 0;
         }
-        col.style.backgroundColor = colors[colorIndex];
+        bodyElement.style.backgroundColor = colors[colorIndex];
         colorIndex++;
     }
